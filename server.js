0a3d48bc69c46910fad78a84280cbffb68c05863
gabrieljim <gabrieljim641@gmail.com>
2019-09-07 16:17:26 -0400

    Server done

--- server.js
+++ server.js
@@ -20,13 +20,17 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+	res.json({
+		ipaddress: req.ip,
+		language: req.get('Accept-Language'),
+		software: req.get('User-Agent')
+	})
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
